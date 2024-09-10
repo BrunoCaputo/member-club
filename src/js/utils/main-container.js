4e6e60cc2f33f95c2e1d2178a9754a9111ee5ab7
BrunoCaputo <brunopc.dev@gmail.com>
2024-09-09 23:54:52 -0300

    feat: Mobile responsiveness

--- src/js/utils/main-container.js
+++ src/js/utils/main-container.js
@@ -5,10 +5,10 @@ const userDataContainer = document.querySelector("main.existing-user");
 export function invalidUser() {
   noUserText.textContent = "ID inválido!";
   noUserContainer.style.display = "flex";
-  userDataContainer.style.display = "none";
+  userDataContainer.classList.remove("show-user-data");
 }
 
 export function validUser() {
   noUserContainer.style.display = "none";
-  userDataContainer.style.display = "grid";
+  userDataContainer.classList.add("show-user-data");
 }
